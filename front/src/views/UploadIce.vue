5be15397e7301e5eccffde7511516f4312c860e9
Roman <romlink@mail.ru>
2024-06-16 21:02:02 +0300

    ice uploading

--- front/src/views/UploadIce.vue
+++ front/src/views/UploadIce.vue
@@ -10,11 +10,51 @@ import TabMenu from '@/components/TabMenu.vue'
       </v-container>
     </v-app-bar>
 
-    <v-main
-      ><v-file-input label="File input"></v-file-input>
-      <v-btn to="/map">Отправить льды</v-btn></v-main
-    >
+    <v-main >
+        <v-file-input @change="handleFileUpload" label="Выберите XLSX файл с толщиной льдов..."></v-file-input>
+        <v-btn v-if="!loading" @click="uploadFile">Отправить льды</v-btn>
+        <div v-if="loading">Файл загружается...</div>
+    </v-main>
   </v-app>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      loading: false,
+      file: null
+    };
+  },
+
+  methods: {
+    handleFileUpload(event) {
+      this.file = event.target.files[0];
+    },
+    async uploadFile() {
+      this.loading = true;
+
+      let formData = new FormData();
+      formData.append('file', this.file);
+
+      try {
+        await axios.post(import.meta.env.VITE_BACK_URL + '/ice/', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        alert('Прогноз льдов успешно обновлен!');
+      } catch (error) {
+        console.error(error);
+        alert('Ошибка при загрузке файла');
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+};
+</script>
 
 <style lang="scss" scoped></style>
